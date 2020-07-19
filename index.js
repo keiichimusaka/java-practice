b16b1124f4ba798fa6fe4334437bbc4257661943
keiichimusaka <67962391+keiichimusaka@users.noreply.github.com>
2020-07-19 23:30:17 +0900

    initial change

---
+++ index.js
@@ -0,0 +1,30 @@
+let youtuber = {
+  list: {
+    business: {
+      youtuber: [{ name: "しまぶー", age: 20, teachprogramming() {} }],
+      teach() {},
+    },
+    entertaiment: {
+      youtuber: [{ name: "ひかきん" }, { name: "はじめしゃちょー" }],
+      makesmile() {},
+    },
+  },
+  plan() {},
+  uploadvideo() {},
+};
+
+const foo = document.getElementById("foo");
+
+const foowidth = foo.offsetWidth;
+const fooHeight = foo.offsetHeight;
+
+const fooArea = foowidth * foowidth;
+
+const foo = 100,
+  foo2 = 100,
+  foo3 = 1000;
+let bar = 200;
+var baz = 300;
+
+const constnumber = 100;
+const constnumber = 200;
